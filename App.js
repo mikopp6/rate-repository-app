2bd7cd06c122f7cae3526ff8268d72ea25ccde3c
Miko <miko.palojarvi@student.oulu.fi>
2020-09-02 19:26:30 +0300

    10.2 done

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('logging');
   return (
     <View style={styles.container}>
       <Text>Testing, probably going to use emulator</Text>
