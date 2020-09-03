caf6d0cad0b2482e0bbd9422e766af6bad7d7fdb
Miko <miko.palojarvi@student.oulu.fi>
2020-09-03 12:57:12 +0300

    10.3 done

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  console.log('logging');
-  return (
-    <View style={styles.container}>
-      <Text>Testing, probably going to use emulator</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Main from './src/components/Main';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return <Main />;
+};
+
+export default App;
