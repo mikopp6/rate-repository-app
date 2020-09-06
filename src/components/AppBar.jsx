fce40f2a1134b552c9648eed301bab2403c2f331
Miko <miko.palojarvi@student.oulu.fi>
2020-09-06 12:53:26 +0300

    10.6 done

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 
 import Text from './Text';
@@ -27,14 +28,14 @@ const AppBar = () => {
   return (
     <View style={styles.flexContainer}>
       <View style={styles.flexItemA}>
-        <TouchableWithoutFeedback>
+        <Link to="/" component={TouchableOpacity} activeOpacity={0.8}>
           <Text color='textSecondary' fontSize='subHeading' fontWeight='bold'>Repositories</Text>
-        </TouchableWithoutFeedback>
+        </Link>
       </View>
       <View style={styles.flexItemB}>
-        <TouchableWithoutFeedback>
+        <Link to="/signin" component={TouchableOpacity} activeOpacity={0.8}>
           <Text color='textSecondary' fontSize='subHeading' fontWeight='bold'>Sign in</Text>
-        </TouchableWithoutFeedback>
+        </Link>
       </View>
     </View>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Route, Switch, Redirect } from 'react-router-native';
 
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,20 +18,16 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </View>
-    //<NativeRouter>
-    //  <Text style={{ paddingTop: 25, paddingBottom: 10 }} fontSize="heading">Rate Repository App</Text>
-    //  <Link to='/' >
-    //    <Text>Repositories</Text>
-    //  </Link>
-    //  <Link to='/signin' >
-    //    <Text>Sign in</Text>
-    //  </Link>
-//
-    //  <Route path='/signin' component={SignIn} />
-    //  <Route exact path='/' component={RepositoryList} />
-    //</NativeRouter>
   );
 };
 
