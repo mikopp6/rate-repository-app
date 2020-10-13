46b679b8162c1e2f2be06b6d9535ce47e8aee48c
Miko <miko.palojarvi@student.oulu.fi>
2020-10-13 07:35:59 +0300

    10.20 done

--- src/components/SingleRepository.jsx
+++ src/components/SingleRepository.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { FlatList, Text } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { format } from 'date-fns';
 
 import RepositoryItem from "./RepositoryItem";
 import useSingleRepository from '../hooks/useSingleRepository';
+import Text from './Text';
+
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+  mainContainer: {
+    backgroundColor: 'white'
+  },
+  flexContainerA: {
+    flexDirection: 'row',
+    padding: 15
+  },
+  reviewRatingContainer: {
+    padding: 10,
+    height: 50,
+    width: 50,
+    borderWidth: 3,
+    borderColor: 'blue',
+    borderRadius: 50/2
+  },
+  reviewRating: {
+    alignSelf: 'center',
+    color: 'blue',
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  mainInfoContainer: {
+    flexShrink: 1,
+    paddingHorizontal: 10
+  },
+  reviewDate: {
+    flexGrow: 1
+  },
+  reviewTextContainer: {
+    marginTop: 10,
+    flexDirection: 'row'
+  },
+  reviewText: {
+
+  }
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryInfo = ({ repository }) => {
   return (
@@ -13,26 +59,47 @@ const RepositoryInfo = ({ repository }) => {
 
 const ReviewItem = ({ review }) => {
   return (
-    <Text>{review.num}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.flexContainerA}>
+        <View style={styles.reviewRatingContainer}>
+          <Text style={styles.reviewRating}>{review.rating}</Text>
+        </View>
+        <View style={styles.mainInfoContainer}>
+          <Text fontWeight='bold'>{review.user.username}</Text>
+          <Text style={styles.reviewDate}>{format(new Date(review.createdAt), 'dd.MM.yyyy')}</Text>
+          <View style={styles.reviewTextContainer}>
+            <Text style={styles.reviewText}>{review.text}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
   );
 };
 
 const SingleRepository = () => {
   const id = useParams().id;
   const { repository } = useSingleRepository(id);
-  const reviews = [{num: '1'},{num: '2'},{num: '3'}];
 
   const singleRepo = repository
     ? repository
+    : [];
+  
+  const reviews = repository
+    ? singleRepo.reviews.edges.map(edge => edge.node)
     : [];
 
   return (
     <FlatList
       data={reviews}
+      ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <ReviewItem review={item} />}
       keyExtractor={({ id }) => id}
-      ListHeaderComponent={() => <RepositoryInfo repository={singleRepo} />}
-      // ...
+      ListHeaderComponent={() =>
+        <View>
+          <RepositoryInfo repository={singleRepo} />
+          <View style={styles.separator}></View>
+        </View>
+      }
     />
   );
 };
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -33,6 +33,20 @@ export const GET_SINGLE_REPOSITORY = gql`
       reviewCount
       ratingAverage
       url
+      reviews {
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+        }
+      }
     }
   }
 `;
