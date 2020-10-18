3f5c0e90a341552768f3f9f0e8c2114fec2e041d
Miko <miko.palojarvi@student.oulu.fi>
2020-10-18 10:39:05 +0300

    10.23 done

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Picker } from '@react-native-community/picker';
+
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { useHistory } from 'react-router-dom';
@@ -12,7 +14,20 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+const OrderDropdown = ({ listOrder, setListOrder }) => {
+  return (
+    <Picker
+      selectedValue={listOrder}
+      onValueChange={(value) => setListOrder(value)}
+    >
+      <Picker.Item label= 'Latest repositories' value= 'CREATED_AT/DESC' />
+      <Picker.Item label= 'Highest rated repositories' value= 'RATING_AVERAGE/DESC' />
+      <Picker.Item label= 'Lowest rated repositories' value= 'RATING_AVERAGE/ASC' />
+    </Picker>
+  );
+};
+
+export const RepositoryListContainer = ({ repositories, listOrder, setListOrder }) => {
   const history = useHistory();
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
@@ -26,6 +41,9 @@ export const RepositoryListContainer = ({ repositories }) => {
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={() =>
+        <OrderDropdown listOrder={listOrder} setListOrder={setListOrder} />
+      }
       renderItem={({ item }) =>
         <TouchableOpacity activeOpacity={0.8} onPress={() => handleSelect(item.id)}>
           <RepositoryItem item={item} isSingleView={false}/>
@@ -36,9 +54,10 @@ export const RepositoryListContainer = ({ repositories }) => {
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [listOrder, setListOrder] = useState('CREATED_AT/DESC');
+  const { repositories } = useRepositories(listOrder);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return <RepositoryListContainer repositories={repositories} listOrder={listOrder} setListOrder={setListOrder}/>;
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,9 +1,12 @@
 import { useQuery } from '@apollo/react-hooks';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (listOrder) => {
+  const [orderBy, orderDirection] = listOrder.split('/');
+  
   const { data, ...result } = useQuery(GET_REPOSITORIES, {
-    fetchPolicy: 'cache-and-network'
+    fetchPolicy: 'cache-and-network',
+    variables: { orderBy, orderDirection }
   });
   
   return { repositories: data ? data.repositories : undefined, ...result };
